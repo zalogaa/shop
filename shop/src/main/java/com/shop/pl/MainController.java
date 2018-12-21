e48c697dfd7e0fc11728dbb67c4b93765ca537b7
User-Komputer\User <klemst2@wp.pl>
2018-12-21 19:46:31 +0100

    projeekt czysty

--- shop/src/main/java/com/shop/pl/MainController.java
+++ shop/src/main/java/com/shop/pl/MainController.java
@@ -9,18 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@Autowired
-	private UserService userService;
-
 	@GetMapping("/")
 	public String showIndex() {
 
-		userService.addUser(new User("adam", "cos@wp.pl", "haslo"));
-
-		Optional<User> u = userService.findUserById((long) 1);
-		if (u.isPresent()) {
-			System.out.println(u.get().getEmail());
-		}
 		return "index";
 	}
 
